d3d57fd619fdb2c30a1d12eb73c0dd6fcf668f73
Paul Kindler <pa179kin@gmail.com>
2024-04-28 21:15:55 +0200

    add multiple targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,191 +4,210 @@ import org.gradle.internal.logging.text.StyledTextOutput.Style.Failure
 import org.gradle.internal.logging.text.StyledTextOutput.Style.Success
 import org.gradle.internal.logging.text.StyledTextOutputFactory
 import org.gradle.kotlin.dsl.support.serviceOf
-import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.dokka.DokkaConfiguration.Visibility
+import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
-    kotlin("multiplatform") version "1.9.20"
-    kotlin("plugin.serialization") version "1.9.20"
+	kotlin("multiplatform") version "1.9.20"
+	kotlin("plugin.serialization") version "1.9.20"
 
-    id("org.jetbrains.dokka") version "1.9.10"
+	id("org.jetbrains.dokka") version "1.9.10"
 
-    id("com.github.breadmoirai.github-release") version "2.4.1"
-    `maven-publish`
-    signing
+	id("com.github.breadmoirai.github-release") version "2.4.1"
+	`maven-publish`
+	signing
 }
 
 group = "fyi.pauli.prorialize"
-version = "1.1.2"
+version = "1.1.3"
 description = "Kotlin.serialization library for the Minecraft protocol."
 val authors = listOf("btwonion", "kxmpxtxnt")
 val isSnapshot = false
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 kotlin {
-    explicitApi()
+	explicitApi()
 
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
-    }
+	jvmToolchain {
+		languageVersion.set(JavaLanguageVersion.of(8))
+	}
 
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-    }
-    linuxX64()
-    linuxArm64()
-    mingwX64()
-    wasmWasi()
-    js {
-        browser()
-        nodejs()
-    }
+	jvm {
+		compilations.all {
+			kotlinOptions.jvmTarget = "1.8"
+		}
+	}
 
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.1")
-                api("org.jetbrains.kotlinx:kotlinx-io-core:0.3.0")
-            }
-        }
+	iosX64()
+	wasmWasi()
+	linuxX64()
+	mingwX64()
+	iosArm64()
+	macosX64()
+	macosArm64()
+	linuxArm64()
 
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
+	js {
+		browser()
+		nodejs()
+	}
 
-        val jvmTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit5"))
-                implementation("org.junit.jupiter:junit-jupiter-engine:5.10.1")
-            }
-        }
-    }
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				api("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.1")
+				api("org.jetbrains.kotlinx:kotlinx-io-core:0.3.0")
+			}
+		}
+
+		val commonTest by getting {
+			dependencies {
+				implementation(kotlin("test-common"))
+				implementation(kotlin("test-annotations-common"))
+			}
+		}
+
+		val jvmTest by getting {
+			dependencies {
+				implementation(kotlin("test-junit5"))
+				implementation("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+			}
+		}
+	}
 }
 
 tasks {
-    withType<DokkaTask>().configureEach {
-        dokkaSourceSets.configureEach {
-            documentedVisibilities.set(
-                setOf(
-                    Visibility.PUBLIC,
-                    Visibility.PROTECTED,
-                    Visibility.PRIVATE,
-                    Visibility.INTERNAL,
-                    Visibility.PACKAGE
-                )
-            )
-        }
-    }
+	withType<DokkaTask>().configureEach {
+		dokkaSourceSets.configureEach {
+			documentedVisibilities.set(
+				setOf(
+					Visibility.PUBLIC,
+					Visibility.PROTECTED,
+					Visibility.PRIVATE,
+					Visibility.INTERNAL,
+					Visibility.PACKAGE
+				)
+			)
+		}
+	}
 
-    register("publishLibrary") {
-        group = "publishing"
-        dependsOn("githubRelease")
-        dependsOn("publish")
-    }
+	register("publishLibrary") {
+		group = "publishing"
+		dependsOn("githubRelease")
+		dependsOn("publish")
+	}
 }
 
-configure<SigningExtension> {
-    val publications = extensions.getByType<PublishingExtension>().publications
-    val publicationCount = publications.size
-    val message =
-        "The following $publicationCount publication(s) are getting signed: ${publications.map(Named::getName)}"
-    val style = when (publicationCount) {
-        0 -> Failure
-        else -> Success
-    }
-    serviceOf<StyledTextOutputFactory>().create("signing").style(style).println(message)
-    sign(*publications.toTypedArray())
+signing {
+	val publications = extensions.getByType<PublishingExtension>().publications
+	val publicationCount = publications.size
+	val message =
+		"The following $publicationCount publication(s) are getting signed: ${publications.map(Named::getName)}"
+	val style = when (publicationCount) {
+		0 -> Failure
+		else -> Success
+	}
+	serviceOf<StyledTextOutputFactory>().create("signing").style(style).println(message)
+	sign(publishing.publications)
 
-    val signingTasks = tasks.filter { it.name.startsWith("sign") && it.name.endsWith("Publication") }
-    tasks.matching { it.name.startsWith("publish") }.configureEach {
-        signingTasks.forEach {
-            mustRunAfter(it.name)
-        }
-    }
+	val signingTasks = tasks.filter { it.name.startsWith("sign") && it.name.endsWith("Publication") }
+	tasks.matching { it.name.startsWith("publish") }.configureEach {
+		signingTasks.forEach {
+			mustRunAfter(it.name)
+		}
+	}
 }
 
 val githubRepo = "ichor-dev/prorialize"
 githubRelease {
-    token(findProperty("github.token")?.toString())
+	token(System.getenv()["github.token"])
 
-    val (owner, repo) = githubRepo.split('/')
-    owner(owner)
-    repo(repo)
-    tagName("v${project.version}")
-    overwrite(true)
-    targetCommitish("main")
+	val (owner, repo) = githubRepo.split('/')
+	owner(owner)
+	repo(repo)
+	tagName("v${project.version}")
+	overwrite(true)
+	targetCommitish("main")
 }
 
 val dokkaJar: Jar = tasks.create<Jar>("dokkaJar") {
-    group = "documentation"
-    archiveClassifier.set("javadoc")
-    from(tasks.findByName("dokkaHtml"))
+	group = "documentation"
+	archiveClassifier.set("javadoc")
+	from(tasks.findByName("dokkaHtml"))
 }
 
 publishing {
-    repositories {
-        maven {
-            name = "ossrh"
-            credentials(PasswordCredentials::class)
-            setUrl(
-                if (!isSnapshot) "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2"
-                else "https://s01.oss.sonatype.org/content/repositories/snapshots"
-            )
-        }
+	repositories {
+		maven {
+			name = "ossrh"
 
-        // Here for instant availability
-        maven {
-            name = "nyon"
-            url = uri("https://repo.nyon.dev/releases")
-            credentials(PasswordCredentials::class)
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-        }
-    }
+			setUrl(
+				if (!isSnapshot) "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2"
+				else "https://s01.oss.sonatype.org/content/repositories/snapshots"
+			)
 
-    publications {
-        register<MavenPublication>(project.name) {
-            from(components["kotlin"])
+			credentials {
+				username = System.getenv()["OSSRH_USERNAME"]
+				password = System.getenv()["OSSRH_PASSWORD"]
+			}
+			authentication {
+				create<BasicAuthentication>("basic")
+			}
+		}
 
-            this.groupId = project.group.toString()
-            this.artifactId = project.name
-            this.version = version.toString()
+		// Here for instant availability
+		maven {
+			name = "nyon"
+			url = uri("https://repo.nyon.dev/${if (!isSnapshot) "releases" else "snapshots"}")
 
-            pom {
-                name.set(project.name)
-                description.set(project.description)
+			credentials {
+				username = System.getenv()["NYON_USERNAME"]
+				password = System.getenv()["NYON_PASSWORD"]
+			}
 
-                developers {
-                    authors.forEach {
-                        developer { name.set(it) }
-                    }
-                }
+			authentication {
+				create<BasicAuthentication>("basic")
+			}
+		}
+	}
 
-                licenses {
-                    license {
-                        name.set("Apache License 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
+	publications {
+		register<MavenPublication>(project.name) {
+			from(components["kotlin"])
 
-                scm {
-                    connection.set("scm:git:git://github.com/${githubRepo}.git")
-                    url.set("https://github.com/${githubRepo}")
-                }
-            }
-        }
-    }
+			this.groupId = project.group.toString()
+			this.artifactId = project.name
+			this.version = version.toString()
+
+			pom {
+				name.set(project.name)
+				description.set(project.description)
+
+				developers {
+					authors.forEach {
+						developer { name.set(it) }
+					}
+				}
+
+				licenses {
+					license {
+						name.set("Apache License 2.0")
+						url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+					}
+				}
+
+				scm {
+					connection.set("scm:git:git://github.com/${githubRepo}.git")
+					url.set("https://github.com/${githubRepo}")
+				}
+			}
+		}
+	}
 }
 
 configure<PublishingExtension> {
-    publications.withType<MavenPublication>().configureEach { artifact(dokkaJar) }
+	publications.withType<MavenPublication>().configureEach { artifact(dokkaJar) }
 }
