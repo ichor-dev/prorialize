5f3c71cc0f123e3ac68414242324c6e95fdd4dd9
Paul Kindler <pa179kin@gmail.com>
2024-05-10 16:40:03 +0200

    made readVarLong work

--- src/commonMain/kotlin/fyi/pauli/prolialize/serialization/types/primitives/Numbers.kt
+++ src/commonMain/kotlin/fyi/pauli/prolialize/serialization/types/primitives/Numbers.kt
@@ -10,7 +10,7 @@ import kotlin.experimental.or
  * Enum to define the binary format of the number.
  */
 public enum class MinecraftNumberType {
-    DEFAULT, UNSIGNED, VAR
+	DEFAULT, UNSIGNED, VAR
 }
 
 public const val SEGMENT_BITS: Byte = 0x7F.toByte() // 127
@@ -20,90 +20,97 @@ public const val CONTINUE_BIT: Byte = 0x80.toByte() // 128
  * Class to de-/serialize VarInts.
  */
 public object VarIntSerializer {
-    public inline fun readVarInt(
-        readByte: () -> Byte
-    ): Int {
-        var numRead = 0
-        var result = 0
-        var read: Byte
-        do {
-            read = readByte()
-            val value = (read and SEGMENT_BITS).toInt()
-            result = result or (value shl 7 * numRead)
-            numRead++
-            if (numRead > 5) {
-                throw RuntimeException("VarInt is too big")
-            }
-        } while (read and CONTINUE_BIT != 0.toByte())
-        return result
-    }
+	public inline fun readVarInt(
+      readByte: () -> Byte,
+  ): Int {
+		var numRead = 0
+		var result = 0
+		var read: Byte
+		do {
+			read = readByte()
+			val value = (read and SEGMENT_BITS).toInt()
+			result = result or (value shl 7 * numRead)
+			numRead++
+			if (numRead > 5) {
+				throw RuntimeException("VarInt is too big")
+			}
+		} while (read and CONTINUE_BIT != 0.toByte())
+		return result
+	}
 
-    public inline fun writeVarInt(
-        value: Int,
-        writeByte: (Byte) -> Unit,
-    ) {
-        var v = value
-        do {
-            var temp = (v and SEGMENT_BITS.toInt()).toByte()
-            // Note: >>> means that the sign bit is shifted with the rest of the number rather than being left alone
-            v = v ushr 7
-            if (v != 0) {
-                temp = temp or CONTINUE_BIT
-            }
-            writeByte(temp)
-        } while (v != 0)
-    }
+	public inline fun writeVarInt(
+		value: Int,
+		writeByte: (Byte) -> Unit,
+	) {
+		var v = value
+		do {
+			var temp = (v and SEGMENT_BITS.toInt()).toByte()
+			// Note: >>> means that the sign bit is shifted with the rest of the number rather than being left alone
+			v = v ushr 7
+			if (v != 0) {
+				temp = temp or CONTINUE_BIT
+			}
+			writeByte(temp)
+		} while (v != 0)
+	}
 
-    @Suppress("unused")
-    public fun varIntBytesCount(
-        value: Int,
-    ): Int {
-        var counter = 0
-        writeVarInt(value) { counter++ }
-        return counter
-    }
+	@Suppress("unused")
+	public fun varIntBytesCount(
+		value: Int,
+	): Int {
+		var counter = 0
+		writeVarInt(value) { counter++ }
+		return counter
+	}
 }
 
 /**
  * Class to de-/serialize VarLongs.
  */
 public object VarLongSerializer {
-    public inline fun readVarLong(
-        readByte: () -> Byte
-    ): Long {
-        var result = 0L
-        var shift = 0
-        while (shift < 56) {
-            val next = readByte()
-            result = result or ((next.toInt() and 0x7F) shl shift).toLong()
-            if (next >= 0) return result
-            shift += 7
-        }
-        return result or ((readByte().toInt() and 0xFF) shl 56).toLong()
-    }
+	public inline fun readVarLong(
+      readByte: () -> Byte,
+  ): Long {
+      var value: Long = 0
+      var position = 0
+      var currentByte: Byte
 
-    public inline fun writeVarLong(
-        value: Long,
-        writeByte: (Byte) -> Unit,
-    ) {
-        var v = value
-        do {
-            var temp = (v and SEGMENT_BITS.toLong()).toByte()
-            // Note: >>> means that the sign bit is shifted with the rest of the number rather than being left alone
-            v = v ushr 7
-            if (v != 0L) {
-                temp = temp or CONTINUE_BIT
-            }
-            writeByte(temp)
-        } while (v != 0L)
-    }
+      while (true) {
+          currentByte = readByte()
+          value = value or ((currentByte.toInt() and SEGMENT_BITS.toInt()).toLong() shl position)
 
-    @Suppress("unused")
-    public fun varLongBytesCount(
-        value: Long,
-    ): Int {
-        var counter = 0
-        writeVarLong(value) { counter++ }
-        return counter
-    }
+          if ((currentByte.toInt() and CONTINUE_BIT.toInt()) == 0) break
+
+          position += 7
+
+          if (position >= 64) throw RuntimeException("VarLong is too big")
+      }
+
+      return value
+	}
+
+	public inline fun writeVarLong(
+		value: Long,
+		writeByte: (Byte) -> Unit,
+	) {
+		var v = value
+		do {
+			var temp = (v and SEGMENT_BITS.toLong()).toByte()
+			// Note: >>> means that the sign bit is shifted with the rest of the number rather than being left alone
+			v = v ushr 7
+			if (v != 0L) {
+				temp = temp or CONTINUE_BIT
+			}
+			writeByte(temp)
+		} while (v != 0L)
+	}
+
+	@Suppress("unused")
+	public fun varLongBytesCount(
+		value: Long,
+	): Int {
+		var counter = 0
+		writeVarLong(value) { counter++ }
+		return counter
+	}
 }
